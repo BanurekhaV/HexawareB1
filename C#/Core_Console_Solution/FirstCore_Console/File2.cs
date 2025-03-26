6e3644738eacbdfefc7016c36cd50e06c10b4901
BanurekhaV <rekhasairam@hotmail.com>
2025-03-26 16:32:34 +0530

    c# day 1

---
+++ C#/Core_Console_Solution/FirstCore_Console/File2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCore_Console
+{
+    internal class Sample
+    {
+        static void Main()
+        {
+            Console.WriteLine("We have reached Sample class of File 2..");
+            Console.Read();
+        }
+    }
+}
---
+++ C#/Core_Console_Solution/FirstCore_Console/FirstClass.cs
@@ -0,0 +1,38 @@
+﻿namespace FirstCore_Console
+{
+    
+    internal class FirstClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This stays in the same line..");
+            FirstClass firstClass = new FirstClass();
+           // firstClass.AcceptDetails(); // calling instance function thru an object
+            AcceptDetails();   //calling static functions within the class by its name directly
+
+            //calling a static function which is in another class
+            SecondClass.DisplayDetails();
+            Console.ReadKey();
+        }
+
+        public static void AcceptDetails()
+        {
+            string? userFName, userLName;
+            Console.WriteLine("Enter Your Name First and Last Names :");
+            userFName = Console.ReadLine();
+            userLName = Console.ReadLine();
+            Console.WriteLine("Your Full Name is : " + userFName + " " + userLName);  //Concatenation
+            Console.WriteLine("Your Full Name is : {0} {1}", userFName,userLName); //Placeholder
+            Console.WriteLine($"Your Full name is {userFName} {userLName}"); //string interpolation
+        }
+    }
+
+    class SecondClass
+    {
+        public static void DisplayDetails()
+        {
+            Console.WriteLine("Hi ! I am DisplayDetails of Second Class");
+        }
+    }
+}
