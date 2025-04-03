42a9f87123baeb8777568924284de8d69360df2d
BanurekhaV <rekhasairam@hotmail.com>
2025-04-03 11:18:45 +0530

    day 7 c#

---
+++ C#/Day7_CSharp/Day7_CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace Day7_CSharp
+{
+    internal class Program
+    {
+        static SqlConnection con = null;
+        static SqlCommand cmd;
+        static SqlDataReader dr;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            SelectData();
+            Console.Read();
+        }
+
+        public static SqlConnection getConnection()
+        {
+            con = new SqlConnection("data source = Laptop-Tjj7d977;initial catalog = HexaDb;integrated security = true;");
+            con.Open();
+            return con;
+        }
+        public static void SelectData()
+        {
+            //StringBuilder sbq = new StringBuilder();
+            //sbq.Append("select * from emp ")
+            //    .Append("where ");
+            //sbq.Append("sal > 2000 ");
+            con = getConnection();
+            cmd = new SqlCommand("select * from emp1",con);
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0] + " " + dr[1]);
+                Console.WriteLine("Employee Number = " + dr["empno"]);
+                Console.WriteLine("Employee Name = " + dr["ename"]);
+                Console.WriteLine("Employee Job = " + dr[2]);
+                Console.WriteLine("Manager ID = "+ dr["mgr_id"]);
+                Console.WriteLine("Employee Saalry = " + dr[5]);
+
+            }
+        }
+    }
+}
