095568a71b8ec77ccd8b34285f6cec3763cf3925
BanurekhaV <rekhasairam@hotmail.com>
2025-04-15 16:57:30 +0530

    javascript

---
+++ Javascript/Javascript_Prj/Javascript_Prj/JavaScript1.js
@@ -0,0 +1,5 @@
+﻿var x = 'a';
+x = 10;
+window.alert("this is alert");
+document.write("this is display");
+console.log("This is console message");
---
+++ Javascript/Javascript_Prj/Javascript_Prj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Javascript_Prj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
